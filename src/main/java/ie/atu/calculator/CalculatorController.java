eee5119c94c1110b594f6e7a8faec3dc78a38d8b
Denis Costello <G00396309@atu.ie>
2023-09-27 12:54:54 +0100

    Added add and subtracr

--- src/main/java/ie/atu/calculator/CalculatorController.java
+++ src/main/java/ie/atu/calculator/CalculatorController.java
@@ -1,7 +1,20 @@
 package ie.atu.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CalculatorController {
-    @GetMapping
+    @GetMapping("/calculate")
+    public String calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation){
+        if(operation == "add"){
+            return "Total= "+ num1 + num2;
+        }
+        else if (operation == "subtract"){
+            return "operation:" + operation + "total" + num1 + num2;
+        }
+
+        return operation;
+    }
 }
